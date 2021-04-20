d4eb015ce767f49a977dcda99ec7aaff2979f361
talrasha <lixiaozhou725@gmail.com>
2021-04-20 07:10:08 +0300

    Add features

--- StackOveflowOnTools.py
+++ StackOveflowOnTools.py
@@ -24,20 +24,71 @@ STACKEXCHANGE = "https://api.stackexchange.com/"
 VERSION = "2.2/"
 endpoint = STACKEXCHANGE+VERSION+'search/advanced'
 
-params = {
-    "key": key,
-    "pagesize": 100,
-    "page": 1,
-#    "order": "desc",
-    "sort": "votes",
-#    "tagged": "visual-studio-code",
-#    "site": "stackoverflow",
-    "title": "vscode",
-#    "filter": "withbody"
-}
+def getMaxPageNumberStackOverflow(toolname):
+    params = {
+        "key": key,
+        "pagesize": 100,
+        #    "page": 1,
+        #    "order": "desc",
+        "sort": "votes",
+        #    "tagged": "visual-studio-code",
+        "site": "stackoverflow",
+        #    "title": "996.ICU",
+        #    "filter": "withbody"
+    }
+    params['body'] = toolname
+    theQuery = STACKEXCHANGE + VERSION + 'search/advanced'
+    startpage = 1
+    n = 50
+    maxpage = 0
+    while True:
+        params['page'] = n
+        response = requests.get(theQuery, params=params)
+        try:
+            theItemListPerPage = response.json()['items']
+            if len(theItemListPerPage) == 100:
+                n = n*2
+                continue
+            else:
+                if len(theItemListPerPage)!=0:
+                    return n
+                else:
+                    maxpage = n
+                    break
+        except:
+            return 0
+    while True:
+        guesspage = int((maxpage+startpage)/2)
+        params['page'] = guesspage
+        theResult = requests.get(theQuery, params=params)
+        try:
+            theItemListPerPage = theResult.json()['items']
+            if startpage>maxpage:
+                return maxpage
+            if len(theItemListPerPage) < 100 and len(theItemListPerPage)!=0:
+                return guesspage
+            elif len(theItemListPerPage) == 100:
+                startpage = guesspage+1
+            elif len(theItemListPerPage) == 0:
+                maxpage = guesspage-1
+        except:
+            return 0
 
-#response = requests.get(endpoint, params=params)
-#result = response.json()
-#pprint(result)
+def getStackOverFlowQuestionsDAC(toolname):
+    themaxpage = getMaxPageNumberStackOverflow(toolname)
+    params = {
+        "key": key,
+        "pagesize": 100,
+        "sort": "votes",
+        "site": "stackoverflow"
+    }
+    theQuery = STACKEXCHANGE + VERSION + 'search/advanced'
+    params['title'] = toolname
+    params['page'] = 1
+    theResult = requests.get(theQuery, params=params)
+    thejson = theResult.json()
+    pprint(thejson)
 
 
+
+print(getMaxPageNumberStackOverflow("AppDynamics"))
