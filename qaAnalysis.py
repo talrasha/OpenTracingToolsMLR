6b066f25e1abbd97fd90098115f015b663079286
Xiaozhou Li <lixiaozhou725@gmail.com>
2021-04-26 13:46:53 +0300

    add functions

--- qaAnalysis.py
+++ qaAnalysis.py
@@ -8,9 +8,9 @@ import time
 import csv, json
 import itertools
 from difflib import SequenceMatcher
-from PersonalInfo.updateInfo import your_email, getGithubToken, getStackoverflowKey
-import updateFlag
 import datetime
+import matplotlib.pyplot as plt
+import operator
 
 desired_width=320
 pd.set_option('display.width', desired_width)
@@ -19,4 +19,40 @@ pd.set_option('display.max_columns',25)
 
 df_questions = pd.read_csv('questions.csv')
 df_answers = pd.read_csv('answers.csv')
+df_questions.drop_duplicates(subset=['question_id'], inplace=True)
+df_answers.drop_duplicates(subset=['answer_id'], inplace=True)
+
+def displayQuestionAnswerDistribution():
+    a4_dims = (11.7, 8.27)
+    smaller_dim = (8, 4.5)
+    fig, ax = plt.subplots(figsize=smaller_dim)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    df_questions_gbtools = df_questions.groupby('toolname').count()
+    df_questions_gbtools.reset_index(inplace=True)
+    df_answers_gbtools = df_answers.groupby('toolname').count()
+    df_answers_gbtools.reset_index(inplace=True)
+
+    toollabels = df_questions_gbtools.loc[:,'toolname'].values.tolist()
+    toolquestioncount = []
+    toolanswercount =[]
+    for tool in toollabels:
+        toolquestioncount.append(df_questions_gbtools.loc[df_questions_gbtools['toolname']==tool, 'question_id'].values[0])
+        toolanswercount.append(df_answers_gbtools.loc[df_answers_gbtools['toolname']==tool, 'answer_id'].values[0])
+    x = np.arange(len(toollabels))
+    width = 0.35
+    rect1 = ax.bar(x - width/2, toolquestioncount, width, label='Questions')
+    rect2 = ax.bar(x + width/2, toolanswercount, width, label='Answers')
+
+    plt.title("Question and Answer Numbers for each Tool")
+    plt.xlabel("Tools")
+    plt.ylabel("Number")
+    ax.set_xticks(x)
+    ax.set_xticklabels(toollabels)
+    ax.legend()
+    ax.bar_label(rect1, padding=3)
+    ax.bar_label(rect2, padding=3)
+    fig.tight_layout()
+    #plt.savefig('methoddistribution.png', bbox_inches='tight')
+    plt.show()
 
